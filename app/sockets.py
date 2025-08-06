492b19e517a2b857c0a11d721bc4b5d7d970344f
damon <damon.eccles@hotmail.com>
2025-08-06 10:17:59 +1000

    fix added some sockets and moved js into its own file

--- app/sockets.py
+++ app/sockets.py
@@ -24,3 +24,20 @@ def socket_event_handler(socketio):
     def handle_export():
         print("export not read")
 
+    @socketio.on('sync-canvas')
+    def handle_sync_canvas():
+        print("sync")
+
+    @socketio.on('clear-canvas')
+    def handle_clear_canvas():
+        print("clearing canvas")
+        # needs to clear canvas for all and clear history array
+
+    def handle_undo():
+        print("undoing")
+        # Needs to clear canvas and redraw global history except client (who undid) history[-1]
+
+    @socketio.on('mouse-movement')
+    def handle_user_mouse():
+        print("moving")
+        # Gets a .json of user name, room, x and y and displays pointer to other users every 50-100ms
---
+++ app/static/canvas.js
@@ -0,0 +1,113 @@
+const canvas = document.getElementById("room-0");
+const sizeInput = document.getElementById("size");
+const colourSelect = document.getElementById("colour");
+const ctx = canvas.getContext("2d");
+//const undo = document.querySelector('button[name="Undo"]');
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+let drawing = false; // Drawing is disabled when we arnt holding mousedown
+let drawSize = 6;
+let idNum = 0;
+let drawColour = colourSelect.value;
+let stroke = []; // current stroke points
+let history = []; // History used for undo functionality
+ctx.lineCap = "round";
+
+
+// prevents <enter> from resetting the page, not the best fix for it.
+document.querySelector(".selection-form").addEventListener("submit", (e) => {
+ e.preventDefault(); 
+});
+
+// Clears the canvas
+clear.addEventListener("click", () => {
+clear_page();
+});
+
+
+undo.addEventListener("click", () => {
+clear_page();
+console.log(history);
+// might wanna move this into a shared location later.
+//for (int i = 0; i < history.length; i++){
+		
+//} Might be better to wait till websockets are a thing and make this global due to clearing the whole page.
+
+});
+
+
+// Changes the colour event. 
+colourSelect.addEventListener("change", () => {
+drawColour = colourSelect.value;
+});
+
+
+// Changes the size of the brush event.
+sizeInput.addEventListener("change", () => {
+drawSize = parseInt(sizeInput.value);
+});
+
+
+// Hold mouse down and enables drawing.
+canvas.addEventListener("mousedown", () => {
+drawing = true;
+ctx.beginPath();
+});
+
+
+// Stop Drawing when you let go of mouse.
+canvas.addEventListener("mouseup", () => {
+drawing = false;
+
+// For saving keystrokes, used for websockets and undo.
+let currentStroke = {
+	strokeId: generate_stroke_ID(),
+	colour: drawColour,
+	size: drawSize,
+	points: stroke
+}
+stroke = [];
+history.push(currentStroke);
+
+
+});
+
+
+// Drawing mechanic when drawing it true and mouse is moving.
+canvas.addEventListener("mousemove", (e) => {
+if (!drawing) return;
+ctx.lineWidth = drawSize; // Size 
+ctx.strokeStyle = drawColour;	
+
+
+let point = [e.offsetX, e.offsetY]
+
+stroke.push(point)
+
+ctx.lineTo(e.offsetX, e.offsetY);
+ctx.stroke();
+ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+
+function generate_stroke_ID(){
+return idNum++;	
+}
+
+
+function removed_stroke_ID(){
+idNum--;
+}
+
+
+function get_highest_stroke(){
+return history[history.length - 1]
+}
+
+function clear_page(){
+ctx.clearRect(0, 0, canvas.width, canvas.height); //clear page
+}
+
+
