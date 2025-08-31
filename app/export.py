2a1fd381de237eb9deec8eefad836501251ebd6a
DamonINFT1016 <eccdc001@mymail.unisa.edu.au>
2025-08-31 10:29:52 +1000

    Cleaned up code

--- app/export.py
+++ app/export.py
@@ -6,19 +6,21 @@ import io
 from collections import defaultdict
 
 def render_strokes(history, canvasH, canvasW):
-    pdf_buffer = io.BytesIO()
+    pdf_buffer = io.BytesIO() # Used to send a pdf file around, store as io.Bytes object.
     sizeH = 2160
     sizeW = 3840
     c = canvas.Canvas(pdf_buffer, pagesize=(sizeW, sizeH))
 
     scaleX = sizeW / canvasW
     scaleY = sizeH / canvasH
-    scaleAvg = (scaleX + scaleY) / 2
+    scaleAvg = (scaleX + scaleY) / 2 # Scale the size of strokes
+
 
     # Group strokes by page
     pages = defaultdict(list)
     for stroke in history:
         pages[stroke.get("page")].append(stroke)
+
 
     for pageNum in sorted(pages.keys()):
         img = Image.new("RGB", (sizeW, sizeH), "white")
@@ -34,16 +36,16 @@ def render_strokes(history, canvasH, canvasW):
                 x = x0 * scaleX
                 y = y0 * scaleY
 
-                if flag:
-                    draw.line([hx, hy, x, y], fill=colour, width=width)
+                if flag: # If flag is enabled the first point has been drawn, thus we can connect previous point to current point, else we draw a dot for our first point.
+                    draw.line([hx, hy, x, y], fill= colour, width= width)
                 else:
                     r = width // 2
-                    draw.ellipse([x - r, y - r, x + r, y + r], fill=colour)
+                    draw.ellipse([x - r, y - r, x + r, y + r], fill= colour)
                     flag = 1
 
                 hx, hy = x, y
 
-        # CPU-intensive filter
+        # cpu filter so we can spike our cpu higher.
         img = img.filter(ImageFilter.UnsharpMask(radius=5, percent=150, threshold=3))
         c.drawImage(ImageReader(img), 0, 0, width=sizeW, height=sizeH)
         c.showPage()
